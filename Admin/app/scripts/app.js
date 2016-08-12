efc3b5b65c71cfc7bb8cd2e97304df866971a916
bbuzzart <wonkim@bbuzzart.com>
2016-08-12 10:29:06 +0900

    app.js 수정

--- Admin/app/scripts/app.js
+++ Admin/app/scripts/app.js
@@ -11,23 +11,32 @@
 var app = angular.module('webAdminApp', ['ngAnimate','ngCookies','ngFileUpload','ngResource','ngRoute','ngSanitize','ngTouch', 'ui.bootstrap', 'ui.tinymce']);
 
 app.constant('config', {
-    apiUrl: 'http://localhost:8080'
+  //apiUrl: 'http://api.pumpkinfactory.net'
+  apiUrl: 'http://localhost:8080'
 });
 
 app.config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'views/main.html',
-            controller: 'MainCtrl'
-        })
-        .when('/service', {
-            templateUrl: 'views/service.html',
-            controller: 'ServiceCtrl'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/content', {
+      templateUrl: 'views/content.html',
+      controller: 'ContentCtrl'
+    })
+    .when('/banner', {
+      templateUrl: 'views/banner.html',
+      controller: 'BannerCtrl'
+    })
+    .when('/popup', {
+      templateUrl: 'views/popup.html',
+      controller: 'PopupCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 
-    // enable html5Mode for pushstate ('#'-less URLs)
-    $locationProvider.html5Mode(true);
+  // enable html5Mode for pushstate ('#'-less URLs)
+  $locationProvider.html5Mode(true);
 });
